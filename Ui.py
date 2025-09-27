6b177076fe6b0fb7a2d8c4a52747e787739f330e
star <japan.muchcha@gmail.com>
2025-09-27 12:10:40 +0700

    pdate utill

--- Ui.py
+++ Ui.py
@@ -1,1 +1,21 @@
-Ui.py
+from Pyside6 import QtCore ,QtGui ,QtWidgets
+from shiboken6 import wrapTnstance
+import maya.OpenMayaUI as omui
+
+class PrimitiveCreatorDialog(QtWidgets.QDialog) :
+	def __init__(self,parent = None) :
+		super().__init__(parent)
+
+		self.resize(300,300)
+		self.setWindowTitle('Primitivecretor')
+
+	def run() :
+		global ui 
+		
+		try:
+			ui.close
+		except :
+			pass
+		prt = wrapTnstance(int(omui.WQtUill.mainWindow()),QtWidgets.QtWidgets)
+		ui = PrimitiveCreatorDialog(parent = prt)
+		ui.show
